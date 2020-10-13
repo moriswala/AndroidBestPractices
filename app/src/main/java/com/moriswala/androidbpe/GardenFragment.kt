ccd03aa918068f6f8cd11b41f063d27e17ead98f
Yakub <yakub@comspots.com>
2020-10-13 15:55:45 +0300

    test

--- app/src/main/java/com/moriswala/androidbpe/GardenFragment.kt
+++ app/src/main/java/com/moriswala/androidbpe/GardenFragment.kt
@@ -35,7 +35,7 @@ class GardenFragment : Fragment() {
     ): View? {
         var binding = FragmentGardenBinding.inflate(inflater, container, false)
         val adapter = GardenPlantingAdapter()
-        Log.v("","sdfsdf")
+        Log.v("","")
         binding.gardenList.adapter = adapter
 
         binding.addPlant.setOnClickListener {
