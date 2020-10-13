111c9c99a54d6ac4e4116faa0bad4bc9b22f31d8
Yakub <yakub@comspots.com>
2020-10-13 14:55:35 +0300

    test log added

--- app/src/main/java/com/moriswala/androidbpe/GardenFragment.kt
+++ app/src/main/java/com/moriswala/androidbpe/GardenFragment.kt
@@ -1,6 +1,7 @@
 package com.moriswala.androidbpe
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -34,6 +35,7 @@ class GardenFragment : Fragment() {
     ): View? {
         var binding = FragmentGardenBinding.inflate(inflater, container, false)
         val adapter = GardenPlantingAdapter()
+        Log.v("","")
         binding.gardenList.adapter = adapter
 
         binding.addPlant.setOnClickListener {
